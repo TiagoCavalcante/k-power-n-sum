be82a1f4feef8c769892b4398d75440620f00695
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2022-10-20 20:43:41 -0300

    Move code to src/

--- main.py
+++
@@ -1,3 +0,0 @@
-from algorithm import formula
-
-print(f"1^20 + ... + n^20 = {formula(20)}")
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from algorithm import formula
+
+while True:
+  k = int(input('k = '))
+  print(formula(k))
